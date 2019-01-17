47823ead92f2e1d18e2f5e76b5e9dd8c751f1f84
Aaron Strouse <aaronstrouse@gmail.com>
2019-01-17 13:39:42 -0500

    added Job models to admin.py

--- portfolio-project/jobs/admin.py
+++ portfolio-project/jobs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Job
 # Register your models here.
+
+admin.site.register(Job)
+
+# Tells the admin about new model named Job, and this causes Job to show up in admin panel
--- portfolio-project/jobs/models.py
+++ portfolio-project/jobs/models.py
@@ -6,3 +6,7 @@ class Job(models.Model):
   
   image = models.ImageField(upload_to='images/')
   summary = models.CharField(max_length=200)
+
+  # Migrations will create a table with rows and columns
+  #  This should be done whenever a new model is made or changes to a model
+  # python3 manage.py makemigrations
